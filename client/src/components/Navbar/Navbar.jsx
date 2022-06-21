625ec96b7aff8eecdfd5f3045a5d428df8dd185c
Arda Coşkun <m.ardacoskun@gmail.com>
2022-06-21 15:29:04 +0300

    app logo added to navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCrown } from "@fortawesome/free-solid-svg-icons";
 import "./navbar.css";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">EventFinder</span>
+        <div className="navLogoContainer">
+          <FontAwesomeIcon icon={faCrown} className="navLogoIcon" />
+          <span className="logo">EventFinder</span>
+        </div>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
