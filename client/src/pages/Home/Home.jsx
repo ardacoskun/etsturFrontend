58788187554b330fdbbf0fbd7c01e918bdedc802
Arda Coşkun <m.ardacoskun@gmail.com>
2022-06-21 14:11:01 +0300

    styles added to navbar component

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { Navbar } from "../../components";
 import "./home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Home;
