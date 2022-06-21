b59463e541ebadee94aa185d186d2e033100e653
Arda Coşkun <m.ardacoskun@gmail.com>
2022-06-21 13:54:53 +0300

    react router installed and home page file created

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
