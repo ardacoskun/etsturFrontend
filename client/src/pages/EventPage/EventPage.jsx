be14445fd16dc78e44f673491fd6a04ceb637080
Arda Coşkun <m.ardacoskun@gmail.com>
2022-06-22 06:02:59 +0300

    event page structure created

--- client/src/pages/EventPage/EventPage.jsx
+++ client/src/pages/EventPage/EventPage.jsx
@@ -1,7 +1,7 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Footer, Header, Navbar } from "../../components";
-import axios from "axios";
 import "./eventPage.css";
 
 const EventPage = () => {
@@ -11,7 +11,7 @@ const EventPage = () => {
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    const getData = async () => {
+    const fetchReq = async () => {
       try {
         setLoading(true);
         const { data } = await axios.get("/events.json");
@@ -19,27 +19,52 @@ const EventPage = () => {
           (item) =>
             item.category.toLowerCase() === categoryName && item.id == id
         );
-        setFilteredData(filtered);
+        setFilteredData(filtered[0]);
         setLoading(false);
       } catch (error) {
         console.log("Bir hata oluştu! Lütfen daha sonra tekrar deneyin");
       }
     };
-    getData();
+    fetchReq();
   }, [categoryName, id]);
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
+  console.log(filteredData);
+
   return (
     <>
       <Navbar />
       <Header />
       <div className="eventPage">
-        {filteredData.map((item, index) => (
-          <div key={index}>{item.name}</div>
-        ))}
+        <div className="eventPageContainer">
+          <div className="eventPageTop">
+            <img src="" alt="" className="eventPageImg" />
+            <div className="eventPageInfo">
+              <div className="eventPageName"></div>
+              <ol className="eventPageArtists">
+                <li className="eventPageArtist"></li>
+              </ol>
+              <div className="eventPageLocation"></div>
+              <div className="eventPageDateContainer">
+                <div className="eventPageDate"></div>
+                <div className="eventPageTime"></div>
+              </div>
+            </div>
+          </div>
+          <div className="eventPage Middle">
+            <div className="eventPageDesc"></div>
+            <div className="eventPageRules">
+              <ol className="eventPageRules">
+                <li className="eventPageRule"></li>
+              </ol>
+            </div>
+          </div>
+          <div className="eventPageBottom"></div>
+          <div className="eventPageShare"></div>
+        </div>
       </div>
       <Footer />
     </>
