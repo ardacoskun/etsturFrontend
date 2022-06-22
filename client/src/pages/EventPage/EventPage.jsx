a4b66ed4471cb0139fe4c1c5aca9e35fa546038e
Arda Coşkun <m.ardacoskun@gmail.com>
2022-06-22 05:36:57 +0300

    event page datas fetched

--- client/src/pages/EventPage/EventPage.jsx
+++ client/src/pages/EventPage/EventPage.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Footer, Header, Navbar } from "../../components";
+import axios from "axios";
 import "./eventPage.css";
 
 const EventPage = () => {
+  const { categoryName, id } = useParams();
+  const [filteredData, setFilteredData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/events.json");
+        const filtered = data.filter(
+          (item) =>
+            item.category.toLowerCase() === categoryName && item.id == id
+        );
+        setFilteredData(filtered);
+        setLoading(false);
+      } catch (error) {
+        console.log("Bir hata oluştu! Lütfen daha sonra tekrar deneyin");
+      }
+    };
+    getData();
+  }, [categoryName, id]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <Navbar />
       <Header />
-      <div className="eventPage"></div>
+      <div className="eventPage">
+        {filteredData.map((item, index) => (
+          <div key={index}>{item.name}</div>
+        ))}
+      </div>
       <Footer />
     </>
   );
