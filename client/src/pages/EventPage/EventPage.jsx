4db4be53480b9354a8884c7f8cac20268b3c3390
Arda Coşkun <m.ardacoskun@gmail.com>
2022-06-22 06:25:19 +0300

    event page fetched data inserted

--- client/src/pages/EventPage/EventPage.jsx
+++ client/src/pages/EventPage/EventPage.jsx
@@ -1,5 +1,5 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 import { Footer, Header, Navbar } from "../../components";
 import "./eventPage.css";
@@ -19,7 +19,7 @@ const EventPage = () => {
           (item) =>
             item.category.toLowerCase() === categoryName && item.id == id
         );
-        setFilteredData(filtered[0]);
+        setFilteredData(filtered);
         setLoading(false);
       } catch (error) {
         console.log("Bir hata oluştu! Lütfen daha sonra tekrar deneyin");
@@ -32,38 +32,70 @@ const EventPage = () => {
     return <div>Loading...</div>;
   }
 
-  console.log(filteredData);
-
   return (
     <>
       <Navbar />
       <Header />
       <div className="eventPage">
         <div className="eventPageContainer">
-          <div className="eventPageTop">
-            <img src="" alt="" className="eventPageImg" />
-            <div className="eventPageInfo">
-              <div className="eventPageName"></div>
-              <ol className="eventPageArtists">
-                <li className="eventPageArtist"></li>
-              </ol>
-              <div className="eventPageLocation"></div>
-              <div className="eventPageDateContainer">
-                <div className="eventPageDate"></div>
-                <div className="eventPageTime"></div>
+          {filteredData[0] && (
+            <>
+              <div className="eventPageTop">
+                <img
+                  src={filteredData[0].images[0]}
+                  alt={filteredData[0].name}
+                  className="eventPageImg"
+                />
+                <div className="eventPageInfo">
+                  <div className="eventPageName">{filteredData[0].name}</div>
+
+                  <ul className="eventPageArtists">
+                    {filteredData[0].artist &&
+                      filteredData[0].artist.map((item, index) => (
+                        <li className="eventPageArtist" key={index}>
+                          {item}
+                        </li>
+                      ))}
+                  </ul>
+
+                  <div className="eventPageLocation">
+                    {filteredData[0].location}
+                  </div>
+                  <div className="eventPageDateContainer">
+                    <div className="eventPageDate"> {filteredData[0].date}</div>
+                    <div className="eventPageTime"> {filteredData[0].time}</div>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className="eventPage Middle">
-            <div className="eventPageDesc"></div>
-            <div className="eventPageRules">
-              <ol className="eventPageRules">
-                <li className="eventPageRule"></li>
-              </ol>
-            </div>
-          </div>
-          <div className="eventPageBottom"></div>
-          <div className="eventPageShare"></div>
+              <div className="eventPage Middle">
+                <div className="eventPageDesc">
+                  {" "}
+                  {filteredData[0].description}
+                </div>
+                <div className="eventPageRules">
+                  <ul className="eventPageRules">
+                    {filteredData[0].details &&
+                      filteredData[0].details.map((item, index) => (
+                        <li className="eventPageRule" key={index}>
+                          {item}
+                        </li>
+                      ))}
+                  </ul>
+                </div>
+              </div>
+              <div className="eventPageBottom">
+                <ul className="eventPagePrices">
+                  {filteredData[0].price &&
+                    Object.values(filteredData[0].price).map((item, index) => (
+                      <li className="eventPagePrice" key={index}>
+                        {item}
+                      </li>
+                    ))}
+                </ul>
+              </div>
+              <div className="eventPageShare"></div>
+            </>
+          )}
         </div>
       </div>
       <Footer />
