e5ec0b9b3b921873e0a9bb5462a7a975051e9169
Arda Coşkun <m.ardacoskun@gmail.com>
2022-06-22 03:37:41 +0300

    local json events data added to public folder and first single category page request made

--- client/src/pages/SingleCategory/SingleCategory.jsx
+++ client/src/pages/SingleCategory/SingleCategory.jsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./singleCategory.css";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
 const SingleCategory = () => {
-  return <div>SingleCategory</div>;
+  const { categoryName } = useParams();
+  const [filteredData, setFilteredData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("./events.json");
+        const filtered = data.filter(
+          (item) => item.category.toLowerCase() === categoryName
+        );
+        setFilteredData(filtered);
+        setLoading(false);
+      } catch (error) {
+        console.log("Bir hata oluştu! Lütfen daha sonra tekrar deneyin");
+      }
+    };
+    getData();
+  }, []);
+
+  return (
+    <div>
+      {filteredData.map((item, index) => (
+        <div key={index}>{item.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default SingleCategory;
